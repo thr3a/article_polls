314fbf6df26138da211d908f826c2587fb200b5f
thr3a <12jk172@ms.dendai.ac.jp>
2018-04-08 12:14:57 +0900

    fix: 複合キーの管理方法を変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,15 +24,15 @@ set :deploy_to, '/var/www/article_polls'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', "config/master.key")
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
 
 # Default value for default_env is {}
-set :default_env, {
-  rails_master_key: `cat config/master.key`
-}
+# set :default_env, {
+#   rails_master_key: `cat config/master.key`
+# }
 
 set :rbenv_type, :system
 set :rbenv_ruby, '2.3.4'
